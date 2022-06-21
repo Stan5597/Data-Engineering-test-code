a0cf40fd724f3ba8a46d50b6d645664507244f7f
Stan5597 <78567811+Stan5597@users.noreply.github.com>
2022-06-21 10:06:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def zipfile_list():
     z_read = zipfile.ZipFile("zipfile.zip", "r")
     z_write = zipfile.ZipFile("zipfile.zip", "a")
 
-    for filename in z_read.namelist():
+    for filename in z_read.namelist(): # for loop to read file in the zip folder
         print('File:', filename)
         z_read.namelist()
         with z_read.open(filename, "r") as read_files:
@@ -20,11 +20,11 @@ def zipfile_list():
             for line in reader:
                 print(line)
 
-    with z_write.open('Combined.csv', "w") as write:
+    with z_write.open('Combined.csv', "w") as write: # Open the csv file in zip folder then try to write in it
         fieldname = ['Source IP', 'Count']
         csv_writer = csv.DictWriter(write, fieldnames=fieldname, delimiter='\t')
         csv_writer.writeheader
-        for line in reader:
+        for line in reader: # loop through each line in the csv file then write rows to Combined.csv
             csv_writer.writerow(line)
 
     return line, filename
