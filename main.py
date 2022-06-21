f7ada7e4352d79e753475ce91c3a77ddd65ebc9f
Stan5597 <78567811+Stan5597@users.noreply.github.com>
2022-06-21 07:16:20 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+import zipfile
+import codecs
+from csv import writer
+from zipfile import ZipFile
+import os
+
+
+def zipfile_list():
+    z_read = zipfile.ZipFile("zipfile.zip", "r")
+    z_write = zipfile.ZipFile("zipfile.zip", "a")
+
+    for filename in z_read.namelist():
+        print('File:', filename)
+        z_read.namelist()
+        with z_read.open(filename, "r") as read_files:
+            reader = csv.DictReader(codecs.iterdecode(read_files, 'utf-8'))
+            for line in reader:
+                print(line)
+
+    with z_write.open('Combined.csv', "w") as write:
+        fieldname = ['Source IP', 'Count']
+        csv_writer = csv.DictWriter(write, fieldnames=fieldname, delimiter='\t')
+        csv_writer.writeheader
+        for line in reader:
+            csv_writer.writerow(line)
+
+    return line, filename
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    zipfile_list()
